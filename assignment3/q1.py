3daadec33cec01e2ec6b3daa24a84c9f653b7b3e
Jianwei Sun <jian58sun@gmail.com>
2017-03-17 20:56:04 -0400

    Completed q1

--- assignment3/q1.py
+++ assignment3/q1.py
@@ -4,52 +4,79 @@ import matplotlib
 import matplotlib.pyplot as plt
 import pickle
 
-k = 3
+
 
 data = np.load("data2D.npy")
 
 #######################################################################
 #
-# Question 1.2
-#
-#################################
-#
 # Hyperparameters
+#
 epoch = 800
 lr = 0.01
 #
 #######################################################################
-# Create a TF variable with normal-sampled initial values
-mu = tf.Variable(tf.random_normal([k, data.shape[1]], stddev=0.01))
-# x is K x B x D
-x = tf.placeholder(tf.float32, [None, data.shape[1]])
-# xu is now K x B
-xu = tf.reduce_sum(tf.squared_difference(tf.expand_dims(mu,1), tf.expand_dims(x, 0)), axis = 2)
-# xub is now B
-xub = tf.reduce_min(xu, axis = 0)
-# Finally, L is just a scalar
-L = tf.reduce_sum(xub)
+for k in range(1, 6):
+    # Create a TF variable with normal-sampled initial values
+    mu = tf.Variable(tf.random_normal([k, data.shape[1]], stddev=0.01))
+    # x is K x B x D
+    x = tf.placeholder(tf.float32, [None, data.shape[1]])
+    # xu is now K x B
+    xu = tf.reduce_sum(tf.squared_difference(tf.expand_dims(mu,1), tf.expand_dims(x, 0)), axis = 2)
+    # xub is now B
+    xub = tf.reduce_min(xu, axis = 0)
+    # Finally, L is just a scalar
+    L = tf.reduce_sum(xub)
 
-train_step = tf.train.AdamOptimizer(lr, beta1=0.9, beta2=0.99, epsilon=1e-5).minimize(L)
+    closest_k = tf.argmin(xu, axis = 0)
 
-init = tf.global_variables_initializer()
-sess = tf.Session()
-sess.run(init)
+    train_step = tf.train.AdamOptimizer(lr, beta1=0.9, beta2=0.99, epsilon=1e-5).minimize(L)
 
-loss = []
-for i in range(epoch):
-    print("Epoch: ", i)
-    sess.run(train_step, feed_dict={x: data})
-    loss.append(sess.run(L, feed_dict={x: data}))
+    init = tf.global_variables_initializer()
+    sess = tf.Session()
+    sess.run(init)
 
-# Save the result
-pickle.dump(loss, open("q1_loss.pkl", "wb"))
+    B = len(data)
 
-# Visualization
-plot_x = list(range(len(loss)))
-plt.plot(plot_x, loss, label='Loss per Update')
-plt.xlabel('Updates')
-plt.ylabel('Loss')
-plt.title('Q1.1 Loss per Update')
-plt.legend()
-plt.show()
+    loss = []
+    for i in range(epoch):
+        print("Epoch: ", i)
+        sess.run(train_step, feed_dict={x: data})
+        loss.append(sess.run(L, feed_dict={x: data}))
+
+    centers = sess.run(mu)
+    k_distribution = sess.run(closest_k, feed_dict={x: data})
+    clustered_results = []
+    for i in range(k):
+        clustered_results.append([])
+
+    index = 0
+    for which_k in k_distribution:
+        clustered_results[which_k].append(data[index])
+        index += 1
+
+    # Save the result
+    pickle.dump(loss, open("q1_loss.pkl", "wb"))
+
+    # Visualization
+    plot_x = list(range(len(loss)))
+    plt.plot(plot_x, loss, label='Loss per Update')
+    plt.xlabel('Updates')
+    plt.ylabel('Loss')
+    plt.title('Q1.2 Loss per Update')
+    plt.legend()
+    plt.show()
+
+    colors = ('g', 'r', 'b', 'y', 'm')
+    for i in range(k):
+        a = np.array(clustered_results[i])
+        percentage = len(a) / B * 100.
+        lbl = 'Cluster {}: {:0.2f}% of data points'.format(str(i + 1), percentage)
+        plt.plot( a[:,0], a[:,1], colors[i] + '.', label= lbl )
+        plt.plot(centers[i][0], centers[i][1], 'k' + '.', markersize=20)
+    plt.grid(True)
+    plt.xlabel('x1')
+    plt.ylabel('x2')
+    plt.title('Q1.3 Clustered results')
+    plt.legend()
+    plt.show()
