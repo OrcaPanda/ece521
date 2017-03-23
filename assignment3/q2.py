e52581fc6d600b9e5ceee1979732375e812b9714
ray <raymondly357@gmail.com>
2017-03-22 21:04:13 -0400

    part2 update

--- assignment3/q2.py
+++ assignment3/q2.py
@@ -1,62 +1,133 @@
 import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
+import pickle
+
+import math
 
 from utils import *
 
-def log_prob_dens_func(X, mu, sigma):
+def log_prob_dens_func(X, mu, variance):
     B, D = X.get_shape().as_list()
 
-    X = tf.expand_dims(X, 1)
-    mu = tf.expand_dims(mu, 0)
-    sigma = tf.expand_dims(sigma, 0)
+    X = tf.expand_dims(X, 1) # B x K x D
+    mu = tf.expand_dims(mu, 0) # B x K x D
+    variance = tf.expand_dims(variance, 0) # B x K x D
 
-    sX = tf.subtract(X, mu)
-    return tf.multiply(tf.reduce_sum(tf.multiply(tf.square(sX), tf.reciprocal(tf.square(sigma))), 2), -0.5) - D * tf.log(2* pi) / 2 - tf.log(tf.reduce_prod(sigma))
+    sX = tf.subtract(X, mu) # B x K x D
+    eS = tf.reduce_sum(tf.multiply(tf.square(sX), tf.reciprocal(variance)), 2) # B x K
+    return (-D * tf.log(2*math.pi) - tf.log(tf.reduce_prod(variance, 2)) / 2 - eS) / 2 # B x K
 
-def log_prob_clust_var(X, pi, mu, sigma):
-    return log_prob_dens_func(X, mu, sigma) + tf.log(pi) - reduce_logsumexp(log_prob_dens_func(X,mu,sigma) + tf.log(pi), reduction_indices=0)
+def log_prob_clust_var(X, pi, mu, variance):
+    # pi is 1 x K
+    rlse = reduce_logsumexp(log_prob_dens_func(X,mu,variance) + tf.log(pi), reduction_indices=1) # B
+    rlse = tf.expand_dims(rlse, 1) # B x K
+    return log_prob_dens_func(X, mu, variance) + tf.log(pi) - rlse # B x K
 
 k = 3
+epoch = 10000
 
-epoch = 800
-lr = 0.01
+np.random.seed(521)
+tf.set_random_seed(521)
 
-data = np.load("data2D.npy")
+data = np.load("data100D.npy")
 np.random.shuffle(data)
+trainingData = data[0:6666]
+validationData = data[6666:10000]
+lr = 0.01
+D = data.shape[1]
 
-d = data.shape[1]
-std = tf.Variable(tf.random_normal([k, d], stddev=0.01))
-exp_std = tf.exp(std)
+for k in range(4,6):
 
-pi = tf.Variable(tf.random_normal([k,1], stddev=0.01))
-soft_pi = logsoftmax(pi)
+    phi = tf.Variable(tf.random_normal([k, D], stddev=0.01))
+    exp_var = tf.exp(phi) # K x D
 
-# Create a TF variable with normal-sampled initial values
-mu = tf.Variable(tf.random_normal([k, d], stddev=0.01))
+    psi = tf.Variable(tf.random_normal([1, k], stddev=0.01))
+    soft_pi = logsoftmax(psi) # 1 x K
 
-# x is K x B x D
-x = tf.placeholder(tf.float32, [None, d])
-L = tf.reduce_sum(reduce_logsumexp(log_prob_dens_func(x,mu,exp_std) + tf.log(soft_pi), reduction_indices=0))
+    # Create a TF variable with normal-sampled initial values
+    mu = tf.Variable(tf.random_normal([k, D], stddev=0.01))
 
-train_step = tf.train.AdamOptimizer(lr, beta1=0.9, beta2=0.99, epsilon=1e-5).minimize(L)
+    # x is B x D
+    X = tf.placeholder(tf.float32, [None, D])
+    temp = log_prob_dens_func(X, mu, exp_var)
+    L = -tf.reduce_sum(reduce_logsumexp(temp + soft_pi, reduction_indices=1))
 
-init = tf.global_variables_initializer()
-sess = tf.Session()
-sess.run(init)
+    train_step = tf.train.AdamOptimizer(lr, beta1=0.9, beta2=0.99, epsilon=1e-5).minimize(L)
 
-B = len(data)
-loss = []
-for i in range(epoch):
-    print("Epoch: ", i)
-    sess.run(train_step, feed_dict={x: data})
-    loss.append(sess.run(L, feed_dict={x: data}))
+    init = tf.global_variables_initializer()
+    sess = tf.Session()
+    sess.run(init)
 
-# Visualization
-plot_x = list(range(len(loss)))
-plt.plot(plot_x, loss, label='Loss per Update')
-plt.xlabel('Updates')
-plt.ylabel('Loss')
-plt.title('Q2.2.2 Loss per Update')
-plt.legend()
-plt.show()
+    print(log_prob_dens_func(X,mu,exp_var).shape)
+
+    B = len(data)
+    loss = []
+    for i in range(epoch):
+        sess.run(train_step, feed_dict={X: trainingData})
+        loss.append(sess.run(L, feed_dict={X: validationData}))
+
+        nnn = sess.run(mu)
+        www = sess.run(exp_var)
+        mmm = sess.run(soft_pi)
+        if i % 100 == 0:
+            print(i)
+        if i == 158:
+            print("ABL")
+
+        if math.isnan(loss[-1]):
+            abc = sess.run(temp, feed_dict={X: validationData})
+            print("NAN")
+
+        # print("Epoch: ", i, loss[i])
+        # if i % 100 == 0:
+        #     centers = sess.run(mu)
+        #     plt.plot(data[:, 0], data[:, 1], 'b.')
+        #     for i in range(len(centers)):
+        #         plt.plot(centers[i][0], centers[i][1], 'k' + '.', markersize=20)
+        #     plt.show()
+
+    centers = sess.run(mu)
+    std = sess.run(exp_var)
+    pickle.dump((loss, centers, std), open("q2_2_100D_" + str(k) +".pkl", "wb"))
+
+    # print(sess.run(mu))
+    # k = sess.run(tf.exp(log_prob_dens_func(X,mu[0],exp_var)), feed_dict={X: sess.run(mu)})
+    # print(k.shape)
+    # print(k)
+    # sum0, sum1, sum2 = 0,0,0
+    # for x in k:
+    #
+    #     sum0 += x[0]
+    #     sum1 += x[1]
+    #     sum2 += x[2]
+    # print(sum0, sum1, sum2)
+
+    k_distribution = sess.run(tf.argmax(log_prob_dens_func(X,mu,exp_var) + soft_pi, axis=1), feed_dict={X: validationData})
+
+    clustered_results = []
+    for i in range(k):
+        clustered_results.append([])
+
+    index = 0
+    for which_k in k_distribution:
+        clustered_results[which_k].append(validationData[index])
+        index += 1
+
+
+    colors = ('g', 'r', 'b', 'y', 'm')
+    for i in range(k):
+        a = np.array(clustered_results[i])
+        percentage = len(a) / len(validationData) * 100.
+        lbl = 'Cluster {}: {:0.2f}% of data points'.format(str(i + 1), percentage)
+        if len(a)!=0:
+            plt.plot( a[:,0], a[:,1], colors[i] + '.', label= lbl )
+        plt.plot(centers[i][0], centers[i][1], 'k' + '.', markersize=20)
+    plt.grid(True)
+    plt.xlabel('x1')
+    plt.ylabel('x2')
+    plt.title('Q2.2.2 ' + str(k) + ' Clustered results - Validation dataset ' )
+    plt.legend()
+    plt.savefig('2_2_2_K_' + str(k) + '_100_mog.png')
+    plt.clf()
+    print("Loss for k = " + str(k) + " : " + str(loss[-1]))
